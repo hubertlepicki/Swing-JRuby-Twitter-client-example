57b16c45528c8a608a939d3532d06a342e5c0e48
Hubert <hubert.lepicki@gmail.com>
2010-04-02 13:56:56 +0200

    code cleanup

--- main.rb
+++ main.rb
@@ -8,38 +8,37 @@ class QuickTweetWindow < javax.swing.JFrame
 
   def initialize
     super
-    self.getContentPane.setLayout BoxLayout.new(self.getContentPane(), BoxLayout::Y_AXIS)
+    self.getContentPane.setLayout BoxLayout.new(self.getContentPane, BoxLayout::Y_AXIS)
 
-    [ @login_label = JLabel.new("Twitter login:"),
+    [ JLabel.new("Twitter login:"),
       @login = JTextField.new,
-      @password_label = JLabel.new("Twitter password:"),
+      JLabel.new("Twitter password:"),
       @password = JPasswordField.new,
-      @your_tweet_label = JLabel.new("Your tweet:"),
-      @tweet_text = JTextField.new,
-      @ok_button = JButton.new("OK") ].each {|element| self.getContentPane.add element }
+      JLabel.new("Your tweet:"),
+      @tweet = JTextField.new,
+      @ok_button = JButton.new("OK")
+    ].each { |element| self.getContentPane.add element }
+
     self.setDefaultCloseOperation JFrame::EXIT_ON_CLOSE
     self.pack
     self.set_visible true
-    @ok_button.add_action_listener(self)
+    @ok_button.add_action_listener self
   end
 
-  def actionPerformed(evt)
-    send_tweet
-  end
+  def actionPerformed(evt); send_tweet; end
 
   def send_tweet
-    puts "Trying to send a tweet"
-    url = URI.parse('http://api.twitter.com/1/statuses/update.xml')
-    req = Net::HTTP::Post.new(url.path)
+    url = URI.parse 'http://api.twitter.com/1/statuses/update.xml'
+    req = Net::HTTP::Post.new url.path
     req.basic_auth @login.get_text, @password.get_text
-    req.set_form_data({'status'=>@tweet_text.get_text}, ';')
+    req.set_form_data({'status'=>@tweet.get_text}, ';')
     res = Net::HTTP.new(url.host, url.port).start {|http| http.request req }
     case res
     when Net::HTTPSuccess, Net::HTTPRedirection
       JOptionPane.show_message_dialog self, "Your tweet has been sent!"
-      @tweet_text.set_text ""
+      @tweet.set_text ""
     else
-      JOptionPane.show_message_dialog self, "Please check your login details... or something."
+      JOptionPane.show_message_dialog self, "Something went wrong... one kitten just died!"
     end
   end
 end
